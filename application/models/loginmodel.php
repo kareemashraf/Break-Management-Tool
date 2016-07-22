0d9c8f5f567343aa193173d53443f2d346775cc7
Kareem.ashraf <kareem.ashraf.91@gmail.com>
2016-07-23 00:46:23 +0200

    add a couple of queries into functions called by the controller

--- application/models/loginmodel.php
+++ application/models/loginmodel.php
@@ -13,6 +13,50 @@ class Loginmodel extends CI_Model
 		}
 
 	}
+
+	function times($username){
+		$query = $this->db->query('SELECT `time` FROM breaks  WHERE  username = "'.$username.'" and `date`= "'.date('Y-m-d').'" ');
+		
+			        $result =  $query->result();
+			        return $result;
+			        
+	}
+
+	function times_rows(){
+		$query = $this->db->query('SELECT `time` FROM breaks WHERE `date`= "'.date('Y-m-d').'" ');
+	return $query->num_rows();
+
+	}
+
+	function times_rows_user($username){
+		$query = $this->db->query('SELECT `time` FROM breaks WHERE username = "'.$username.'" and `date`= "'.date('Y-m-d').'" ');
+	return $query->num_rows(); 
+
+	}
+
+	function check($username, $time, $ftid, $pims){
+
+	$query = $this->db->query('SELECT * FROM breaks WHERE username = "'.$username.'" and time = "'.$time.'" and date= "'.date('Y-m-d').'" ');
+	return $query->num_rows();
+
+	}
+
+	function add($username, $time, $ftid, $pims){
+	
+		$query = $this->db->query('INSERT INTO breaks (username,`time`,ftid,pims,`date`) VALUES ("'.$username.'","'.$time.'","'.$ftid.'","'.$pims.'","'.date('Y-m-d').'")  ');
+			
+	}
+
+	function remove($username, $time, $ftid, $pims){
+		$query = $this->db->query('DELETE FROM breaks WHERE username = "'.$username.'" and time = "'.$time.'" and date= "'.date('Y-m-d').'" ');
+			
+	}
+
+	function exceed($username, $time, $ftid, $pims){
+		$query = $this->db->query('SELECT `time` FROM breaks  WHERE  username = "'.$username.'" and `date`= "'.date('Y-m-d').'" ');
+
+		return $query->num_rows();
+	}
 }
 
 ?>
